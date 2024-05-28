6182968c2f4a7d4a1c617ec703664e95689b49b0
瀧澤 まりえ <2405064@M31MAC155.local>
2024-05-28 09:24:52 +0900

    first commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by 瀧澤 まりえ on 2024/05/28.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
