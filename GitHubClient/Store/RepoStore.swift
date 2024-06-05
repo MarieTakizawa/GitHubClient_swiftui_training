e86594f8bf3c553602ee0619deeb5e1ac0219b9f
Marie Takizawa <marie.takizawa@rizapgroup.com>
2024-06-05 11:57:53 +0900

    3.1. Single Source of Truth

---
+++ GitHubClient/Store/RepoStore.swift
@@ -0,0 +1,24 @@
+import Observation
+
+@Observable
+final class ReposStore {
+    enum Action {
+        case onAppear
+        case onRetryButtonTapped
+    }
+    private(set) var state: Stateful<[Repo]> = .loading
+    
+    func send(_ action: Action) async {
+        switch action {
+        case .onAppear, .onRetryButtonTapped:
+            state = .loading
+            
+            do {
+                let repos = try await RepoAPIClient().getRepos()
+                state = .loaded(repos)
+            } catch {
+                state = .failed(error)
+            }
+        }
+    }
+}
